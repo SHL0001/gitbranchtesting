ec363122e1e23e36f0e42da1e8a1792584d8780e
SHL0001 <sohail.khalid@activekeysolutions.com>
2021-09-09 14:33:41 +0500

    Subtraction function is added

--- cal.cpp
+++ cal.cpp
@@ -6,13 +6,34 @@ double add(double a, double b)
 	double c = a+b;
 	return c;
 }
+double sub(double a, double b)
+{
+        double c = a-b;
+        return c;
+}
+
 int main()
 {
 double x,y;
+char op;
 cout<<"Enter first number: ";
 cin>>x;
+cout<<"Enter which opertion you want to perform +,-,*,/";
+cin>>op
 cout<<"Enter second number: ";
 cin>>y;
+if(op='+')
+{
+ add(x,y);
+}
+else if(op='-')
+{
+ sub(x,y);
+}
+else
+{
+cout<<"This part of calculator is not handled well now, Sorry";
+}
 
 return 0;
 }
